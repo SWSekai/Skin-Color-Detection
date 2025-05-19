2e9ce4078bbafdb98933565ed33a65c85a752236
Hans Chen <hanschen31310@gmail.com>
2025-05-20 00:46:13 +0800

    05/19 錯誤參數改正

--- method.py
+++ method.py
@@ -49,7 +49,7 @@ def skin_detect(image_path, ground_truth_path):
                 skin_mask[y, x] = 0
     
     # 後處理
-    kernel = np.ones((5, 5), np.uint8)
+    kernel = np.ones((3, 3), np.uint8)
     skin_mask = cv2.morphologyEx(skin_mask, cv2.MORPH_OPEN, kernel, iterations=2) # 開運算
     skin_mask = cv2.morphologyEx(skin_mask, cv2.MORPH_CLOSE, kernel, iterations=2) # 閉運算
         
