93546dcbd04a9b28195f51b3e87aced01273e842
Hans Chen <hanschen31310@gmail.com>
2025-05-19 20:40:27 +0800

    05/19方法更新，大致完成

---
+++ main.py
@@ -0,0 +1,59 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+from method import skin_detect
+
+def createWindow(image = None, title = None): 
+    """
+    創建一個圖形視窗
+    """
+    plt.figure(figsize=(10, 6))
+    
+    if image is not None:
+        plt.imshow(image, cmap='gray')
+        
+    if title is not None:
+        plt.title(title)
+    
+    # 去除圖形標框
+    plt.gca().spines['top'].set_visible(False)
+    plt.gca().spines['right'].set_visible(False)
+    plt.gca().spines['left'].set_visible(False)
+    plt.gca().spines['bottom'].set_visible(False)
+    
+    plt.xticks([]), plt.yticks([]) # 隱藏坐標軸刻度
+
+def main():
+    sum_iou = 0
+    sum_iou = 0
+    
+    for i in range(1, 7):
+        image_path = f"./Photo/pic{i}.jpg"
+        ground_truth_path = f"./GroundTruth/pic{i}.png"
+        origin_result_path = f"./Result/pic{i}.jpg"
+        
+        skin_masked, iou = skin_detect(image_path, ground_truth_path)
+
+        print(f"picture{i} image\'s IOU: {iou}")
+
+        
+        createWindow(title=f"picture{i} image\'s IOU: {iou.round(4)}")
+        
+        plt.subplot(1, 3, 1), plt.title("Image"), plt.imshow(cv2.imread(image_path)), plt.axis("off"), plt.xticks([]), plt.yticks([])
+        plt.subplot(1, 3, 2), plt.title("Ground truth"), plt.imshow(cv2.imread(ground_truth_path, cv2.IMREAD_GRAYSCALE), cmap="gray"), plt.axis("off"), plt.xticks([]), plt.yticks([])
+        plt.subplot(1, 3, 3), plt.title("Result"), plt.imshow(skin_masked, cmap="gray"), plt.axis("off"), plt.xticks([]), plt.yticks([])
+        
+        plt.show()
+        
+        cv2.imwrite(f"./Result/pic{i}.jpg", skin_masked)
+        
+        sum_iou += iou
+    
+    average_iou = sum_iou/6
+    
+    print(f"\nAverage IOU is: {average_iou}")
+
+if __name__ == "__main__":
+
+    main()
--- method.py
+++ method.py
@@ -1,57 +1,69 @@
 import cv2
 import numpy as np
 
-def skin_detect_optimized(image_path, ground_truth_path=None):
-    
+def is_skin(r, g, b, h, s, y, cr, cb):
+    """
+    判斷一個像素是否為膚色。
+    """
+    rgb_condition = (
+        r > 100 and g > 45 and b > 30 and r > g and r > b and abs(r - g) > 15
+    )
+    hsv_condition = s > 35
+    ycbcr_condition = (
+        cr > 145
+        and cb > 75
+        and y > 80
+        and cr <= (1.9 * cb) + 40
+        and cr >= (0.2448 * cb) + 7.209
+        and cr >= (-4.5652 * cb) + 234.5652
+        and cr <= (-1.25 * cb) + 301.75
+        and cr <= (-2.2857 * cb) + 432.85
+    )
+
+    return rgb_condition and hsv_condition and ycbcr_condition
+
+
+def skin_detect(image_path, ground_truth_path):
     image = cv2.imread(image_path)
-    if ground_truth_path:
-        ground_truth = cv2.imread(ground_truth_path, cv2.IMREAD_GRAYSCALE)
+    ground_truth = cv2.imread(ground_truth_path, cv2.IMREAD_GRAYSCALE)
+    
+    image = cv2.GaussianBlur(image, (3, 3), 0)
 
-    # 影像前處理 (平滑)
-    image_blurred = cv2.GaussianBlur(image, (5, 5), 0)
+    height, width, channels = image.shape
 
-    # 顏色空間轉換
-    image_hsv = cv2.cvtColor(image_blurred, cv2.COLOR_BGR2HSV)
-    image_ycbcr = cv2.cvtColor(image_blurred, cv2.COLOR_BGR2YCrCb)
+    skin_mask = np.zeros((height, width), dtype=np.uint8)
 
-    # 膚色檢測 (論文中的條件 + 條件組合)
-    skin_mask = np.zeros_like(image, dtype=np.uint8)[:, :, 0]  # 單通道遮罩
+    for y in range(height):
+        for x in range(width):
+            b, g, r = image[y, x]
 
-    for i in range(image.shape[0]):
-        for j in range(image.shape[1]):
-            r, g, b = image[i, j, 2], image[i, j, 1], image[i, j, 0]
-            h, s, _ = image_hsv[i, j]
-            y, cr, cb = image_ycbcr[i, j]
+            hsv = cv2.cvtColor(np.uint8([[[b, g, r]]]), cv2.COLOR_BGR2HSV)[0][0]
+            h, s, v = hsv[0], hsv[1], hsv[2]
 
-            # 論文中的膚色條件
-            rgb_condition = r > 97 and g > 40 and b > 20 and r > g and r > b and abs(int(r) - int(g)) > 15
-            hsv_condition = 0.0 <= h <= 50.0 and 0.23 <= s <= 0.68
-            ycbcr_condition = cr > 135 and cb > 85 and y > 80 and cr <= (1.5862 * cb) + 20 and cr >= (0.3448 * cb) + 76.2069 and cr >= (-4.5652 * cb) + 234.5652 and cr <= (-1.15 * cb) + 301.75 and cr <= (-2.2857 * cb) + 432.85
+            ycbcr = cv2.cvtColor(np.uint8([[[b, g, r]]]), cv2.COLOR_BGR2YCrCb)[0][0]
+            yc, cr, cb = ycbcr[0], ycbcr[1], ycbcr[2]
+            
+            if is_skin(r, g, b, h, s, yc, cr, cb):
+                skin_mask[y, x] = 255
+            else:
+                skin_mask[y, x] = 0
+    
+    # 後處理
+    kernel = np.ones((5, 5), np.uint8)
 
-            # 條件組合
-            if rgb_condition or (hsv_condition and ycbcr_condition):
-                skin_mask[i, j] = 255
+    skin_mask = cv2.morphologyEx(skin_mask, cv2.MORPH_OPEN, kernel, iterations=2) # 開運算
+    skin_mask = cv2.morphologyEx(skin_mask, cv2.MORPH_CLOSE, kernel, iterations=2) # 閉運算
+        
+    skin_mask = cv2.GaussianBlur(skin_mask, (3, 3), 0)
+            
+    iou = calculate_iou(skin_mask, ground_truth)
 
-    # 後處理 (形態學操作)
-    kernel = np.ones((3, 3), np.uint8)
-    skin_mask_opened = cv2.morphologyEx(skin_mask, cv2.MORPH_OPEN, kernel, iterations=2)
-    skin_mask_closed = cv2.morphologyEx(skin_mask_opened, cv2.MORPH_CLOSE, kernel, iterations=2)
-
-    # 邊緣檢測
-    edges = cv2.Canny(image_blurred, 100, 200)
-    skin_mask = cv2.bitwise_or(skin_mask_closed, edges)  # 將邊緣加入遮罩
-
-    # 效能評估
-    iou = None
-    if ground_truth_path:
-        iou = calculate_iou(ground_truth, skin_mask_closed)
-
-    return skin_mask_closed, iou
-
+    return skin_mask, iou
 
 def calculate_iou(mask1, mask2):
     """計算兩個二值化遮罩的 IOU"""
     intersection = np.logical_and(mask1, mask2)
     union = np.logical_or(mask1, mask2)
     iou = np.sum(intersection) / np.sum(union)
+    
     return iou
